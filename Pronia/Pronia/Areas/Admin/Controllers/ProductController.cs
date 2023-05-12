dfcba50f23facebd2222403d1ff819fda040be9d
Konul Ibrahimova <konulsi@code.edu.az>
2023-05-12 16:54:20 +0400

    added product's create

--- Pronia/Pronia/Areas/Admin/Controllers/ProductController.cs
+++ Pronia/Pronia/Areas/Admin/Controllers/ProductController.cs
@@ -153,5 +153,201 @@ namespace Pronia.Areas.Admin.Controllers
 
 
 
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+
+            ViewBag.categories = await GetCategoryAsync();
+            ViewBag.tags = await GetTagAsync();
+            ViewBag.sizes = await GetSizeAsync();
+            ViewBag.colors = await GetColorAsync();
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductCreateVM model)
+        {
+            try
+            {
+                ViewBag.categories = await GetCategoryAsync();
+                ViewBag.tags = await GetTagAsync();
+                ViewBag.sizes = await GetSizeAsync();
+                ViewBag.colors = await GetColorAsync();
+
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+
+                Product newProduct = new();
+                List<ProductImage> productImages = new();
+                List<ProductCategory> productCategories= new();
+                List<ProductTag> productTags= new();
+                List<ProductSize> productSizes= new();
+
+
+                //main image
+                if (!model.MainImage.CheckFileType("image/"))
+                {
+                    ModelState.AddModelError("Photo", "File type must be image");
+                    return View();
+                }
+
+                if (!model.MainImage.CheckFileSize(200))
+                {
+                    ModelState.AddModelError("Photo", "Image size must be max 200kb");
+                    return View();
+                }
+                string mainImagefileName = Guid.NewGuid().ToString() + "_" + model.MainImage.FileName;
+                string mainImagepath = FileHelper.GetFilePath(_env.WebRootPath, "assets/images/website-images", mainImagefileName);
+                await FileHelper.SaveFileAsync(mainImagepath, model.MainImage);
+                newProduct.MainImage = mainImagefileName;
+
+
+                //hover image
+                if (!model.HoverImage.CheckFileType("image/"))
+                {
+                    ModelState.AddModelError("Photo", "File type must be image");
+                    return View();
+                }
+
+                if (!model.HoverImage.CheckFileSize(200))
+                {
+                    ModelState.AddModelError("Photo", "Image size must be max 200kb");
+                    return View();
+                }
+                string hoverImagefileName = Guid.NewGuid().ToString() + "_" + model.HoverImage.FileName;
+                string hoverImagepath = FileHelper.GetFilePath(_env.WebRootPath, "assets/images/website-images", hoverImagefileName);
+                await FileHelper.SaveFileAsync(hoverImagepath, model.HoverImage);
+                newProduct.HoverImage =hoverImagefileName;
+
+                //all images
+                foreach (var photo in model.Photos)
+                {
+                    if (!photo.CheckFileType("image/"))
+                    {
+                        ModelState.AddModelError("Photo", "File type must be image");
+                        return View();
+                    }
+
+                    if (!photo.CheckFileSize(500))
+                    {
+                        ModelState.AddModelError("Photo", "Image size must be max 200kb");
+                        return View();
+                    }
+                }
+
+
+                foreach (var photo in model.Photos)
+                {
+                    string fileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                    string path = FileHelper.GetFilePath(_env.WebRootPath, "assets/images/website-images", fileName);
+                    await FileHelper.SaveFileAsync(path, photo);
+
+                    ProductImage productImage = new()
+                    {
+                        Image = fileName
+                    };
+
+                    productImages.Add(productImage);
+                }
+                newProduct.Images= productImages;
+
+                if (model.CategoryIds.Count > 0)
+                {
+                    foreach (var cateId in model.CategoryIds)
+                    {
+                        ProductCategory productCategory = new()
+                        {
+                            CategoryId = cateId
+                        };
+
+                        productCategories.Add(productCategory);
+                    }
+                    newProduct.ProductCategories = productCategories;
+                }
+                else
+                {
+                    ModelState.AddModelError("CategoryIds", "Don't be empty");
+                    return View();
+                }
+
+
+
+                if (model.TagIds.Count > 0)
+                {
+                    foreach (var tagId in model.TagIds)
+                    {
+                        ProductTag productTag = new()
+                        {
+                            TagId = tagId
+                        };
+
+                        productTags.Add(productTag);
+                    }
+                    newProduct.ProductTags = productTags;
+                }
+                else
+                {
+                    ModelState.AddModelError("TagIds", "Don't be empty");
+                    return View();
+                }
+
+
+
+                if (model.SizeIds.Count > 0)
+                {
+                    foreach (var sizeId in model.SizeIds)
+                    {
+                        ProductSize productSize = new()
+                        {
+                            SizeId = sizeId
+                        };
+
+                        productSizes.Add(productSize);
+                    }
+                    newProduct.ProductSizes = productSizes;
+                }
+                else
+                {
+                    ModelState.AddModelError("TagIds", "Don't be empty");
+                    return View();
+                }
+
+                var convertPrice = decimal.Parse(model.Price);
+                Random random = new();
+
+                newProduct.Name= model.Name;
+                newProduct.Description= model.Description;
+                newProduct.Price= convertPrice;
+                newProduct.StockCount= model.StockCount;
+                newProduct.SaleCount= model.SaleCount;
+                newProduct.Rate= model.Rate;
+                newProduct.ColorId= model.ColorId;
+                newProduct.SKU= model.Name.Substring(0,3) + "_" + random.Next();
+
+                //await _context.ProductImages.AddRangeAsync(productImages);
+                await _context.Products.AddAsync(newProduct);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.error = ex.Message;
+                return View();
+            }
+
+
+        }
+
+
     }
 }
